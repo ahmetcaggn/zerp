b6b8f109c09dda81c9b7df1d8f33d951056b0629
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2026-03-12 15:55:01 +0300

    fix: remove unused spring-boot-starter-security dependency from common module pom.xml

--- zerp-backend/notification/src/main/java/org/zerp/notification/NotificationApplication.java
+++ zerp-backend/notification/src/main/java/org/zerp/notification/NotificationApplication.java
@@ -3,17 +3,10 @@ package org.zerp.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.actuate.web.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {
-        SecurityAutoConfiguration.class,
-        UserDetailsServiceAutoConfiguration.class,
-        ManagementWebSecurityAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EntityScan("org.zerp.common")
