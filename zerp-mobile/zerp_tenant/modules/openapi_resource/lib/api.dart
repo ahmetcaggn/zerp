3d372c05989cc004a66b8fe6e51f0495d2a65bd9
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2026-04-16 19:08:45 +0300

    feat(openapi): add resource, sale, and suggestion API specifications and generation scripts

---
+++ zerp-mobile/zerp_tenant/modules/openapi_resource/lib/api.dart
@@ -0,0 +1,18 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+library openapi.api;
+
+export 'package:dart_network_layer_core/dart_network_layer_core.dart';
+
+export 'base/base_request.dart';
+export 'base/api_config.dart';
+
+
+
