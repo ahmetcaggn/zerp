8752ce7918ef7bc1e779cc7413d0437d5bd73ad7
ahmetcaggn <ahmetcannersoy@gmail.com>
2026-04-27 03:58:40 +0300

    feat: replace MuiEmotionCacheProvider with AppRouterCacheProvider and update dependencies

--- zerp-frontend/zerp-tenant/src/core/providers/app-providers.tsx
+++ zerp-frontend/zerp-tenant/src/core/providers/app-providers.tsx
@@ -1,9 +1,9 @@
 'use client'
 
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
 import { AuthProvider } from '@/core/auth/client/auth-provider'
 import { I18nProvider } from '@/core/i18n/i18n-provider'
 import type { MessageDictionary } from '@/core/i18n/messages'
-import { MuiEmotionCacheProvider } from '@/core/providers/mui-emotion-cache-provider'
 import { AppQueryProvider } from '@/core/providers/query-provider'
 import { AppThemeProvider } from '@/core/providers/theme-provider'
 import { ToastProvider } from '@/core/providers/toast-provider'
@@ -20,7 +20,7 @@ export function AppProviders({
 }) {
   return (
     <AppQueryProvider>
-      <MuiEmotionCacheProvider>
+      <AppRouterCacheProvider>
         <AuthProvider>
           <I18nProvider locale={locale} messages={messages}>
             <AppThemeProvider>
@@ -28,7 +28,7 @@ export function AppProviders({
             </AppThemeProvider>
           </I18nProvider>
         </AuthProvider>
-      </MuiEmotionCacheProvider>
+      </AppRouterCacheProvider>
     </AppQueryProvider>
   )
 }
