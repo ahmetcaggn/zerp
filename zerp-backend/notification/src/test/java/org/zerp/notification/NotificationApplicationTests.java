4b75004c2008d5bcf746859d987a89afef616732
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2026-05-05 19:30:33 +0300

    test: remove all context load tests

--- zerp-backend/notification/src/test/java/org/zerp/notification/NotificationApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package org.zerp.notification;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NotificationApplicationTests {
-    // Context loads is removed for fail duo to missing mail credential.
-}
