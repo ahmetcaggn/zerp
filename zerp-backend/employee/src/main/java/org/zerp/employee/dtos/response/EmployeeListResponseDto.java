807b77234414b104f706b130ec5b6625c7a466d0
Faruk Emre EKŞİOĞLU <faruk.eksioglu@hotmail.com>
2026-05-16 01:24:24 +0300

    feat: add username field to EmployeeListResponseDto and EmployeeResponseDto

--- zerp-backend/employee/src/main/java/org/zerp/employee/dtos/response/EmployeeListResponseDto.java
+++ zerp-backend/employee/src/main/java/org/zerp/employee/dtos/response/EmployeeListResponseDto.java
@@ -7,16 +7,11 @@ import java.util.UUID;
 
 @Data
 public class EmployeeListResponseDto {
-
     private UUID id;
-
+    private String username;
     private String firstName;
-
     private String lastName;
-
     private String email;
-
     private String phoneNumber;
-
     private EmploymentStatus status;
 }
